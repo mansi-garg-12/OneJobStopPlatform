c5c5baf459784c1ab037a08e8335979301849205
mansi Garg <gargmansi0307@gmail.com>
2025-06-19 21:10:14 +0530

    Basic Setup for Frontend and Backend

---
+++ oneJobStop-Backend/src/main/java/com/org/oneJobStop/OneJobStopApplication.java
@@ -0,0 +1,13 @@
+package com.org.oneJobStop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneJobStopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneJobStopApplication.class, args);
+	}
+
+}
